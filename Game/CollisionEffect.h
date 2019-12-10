5d188734b1f6a529cbf78ab1e820d20a568cded6
lahdirakram <popololo5456>
2019-12-10 14:04:46 +0100

    Some bug fixes

--- Game/CollisionEffect.h
+++ Game/CollisionEffect.h
@@ -5,6 +5,8 @@
 #ifndef GAME_COLLISIONEFFECT_H
 #define GAME_COLLISIONEFFECT_H
 
+#include "Element.h"
+
 class CollisionEffect
 {
     virtual bool onCollision(Element element) = 0;
--- Game/Streumon.cpp
+++
@@ -1,5 +0,0 @@
-//
-// Created by netbook on 10/12/2019.
-//
-
-#include "Streumon.h"
